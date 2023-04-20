72339f701949735c8e6c1f570d2b2b24d15cd962
Víctor Galiano <victoradonay.galianopombo@gmail.com>
2023-04-20 21:04:32 +0200

    a

--- Recursos/modeloBD/diseño fisico.sql
+++ Recursos/modeloBD/diseño fisico.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE TABLE aviones (
 
 
 -- -----------------------------------------------------
--- Tabla trayectos
+-- Table trayectos
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE trayectos (
   idTrayecto INT AUTO_INCREMENT,
@@ -30,7 +30,7 @@ CREATE OR REPLACE TABLE trayectos (
 
 
 -- -----------------------------------------------------
--- Tabla miembros
+-- Table miembros
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE miembros (
   idTripulacion INT AUTO_INCREMENT,
@@ -50,7 +50,7 @@ CREATE OR REPLACE TABLE miembros (
 
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Tabla pasajeros
+-- Table pasajeros
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE pasajeros (
   idPasajeros INT AUTO_INCREMENT,
@@ -66,7 +66,7 @@ CREATE OR REPLACE TABLE pasajeros (
 );
 
 -- -----------------------------------------------------
--- Tabla vuelos
+-- Table vuelos
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE vuelos (
   idVuelo INT AUTO_INCREMENT,
@@ -81,17 +81,15 @@ CREATE OR REPLACE TABLE vuelos (
     REFERENCES aviones (idAvion)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    -- Hemos restringido el comportamiento de esta FK porque no se pueden borrar aviones
   CONSTRAINT fk_vuelos_trayectos
     FOREIGN KEY (idTrayecto)
     REFERENCES trayectos (idTrayecto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
-    -- Hemos restringido el comportamiento de esta FK porque no se pueden borrar trayectos
 );
 
 -- -----------------------------------------------------
--- Tabla Pasajeros_vuelos
+-- Table Pasajeros_vuelos
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE Pasajeros_vuelos (
   idVuelo INT,
@@ -103,7 +101,6 @@ CREATE OR REPLACE TABLE Pasajeros_vuelos (
     REFERENCES vuelos (idVuelo, idAvion)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    -- Podemos establecer comportamiento en cascada sin preocuparnos de la propagación ya que esta es una tabla intermedia
   CONSTRAINT fk_vuelos_has_pasajeros_pasajeros
     FOREIGN KEY (idPasajeros)
     REFERENCES pasajeros (idPasajeros)
@@ -115,7 +112,7 @@ CREATE OR REPLACE TABLE Pasajeros_vuelos (
 
 
 -- -----------------------------------------------------
--- Tabla miembros_vuelos
+-- Table miembros_vuelos
 -- -----------------------------------------------------
 CREATE OR REPLACE TABLE miembros_vuelos (
   idVuelo INT,
@@ -127,8 +124,6 @@ CREATE OR REPLACE TABLE miembros_vuelos (
     REFERENCES vuelos (idVuelo, idAvion)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    -- Podemos establecer comportamiento en cascada sin preocuparnos de la propagación ya que esta es una tabla intermedia
-    -- Tras consultar con el profesor de programación, hemos decidido que esta tabla será análoga a la tabla Asignados del enunciado
   CONSTRAINT fk_vuelos_has_miembros_miembros
     FOREIGN KEY (idTripulacion)
     REFERENCES miembros (idTripulacion)
@@ -137,3 +132,17 @@ CREATE OR REPLACE TABLE miembros_vuelos (
 	 );
 
 
+INSERT INTO miembros (idTripulacion, ecorreo, nombre, apellido1, apellido2, direccion, categoria, fechaNacimiento, telefono) VALUES
+    (1, 'juan.perez@mail.com', 'Juan', 'Perez', 'Gonzalez', 'Calle Mayor 1', 'azafato', '1990-01-01', '123456789'),
+    (2, 'ana.sanchez@mail.com', 'Ana', 'Sanchez', 'Garcia', 'Plaza Mayor 2', 'piloto', '1985-02-15', '987654321'),
+    (3, 'pedro.lopez@mail.com', 'Pedro', 'Lopez', 'Martinez', 'Calle del Sol 3', 'copiloto', '1992-06-03', '654321987'),
+    (4, 'maria.rodriguez@mail.com', 'Maria', 'Rodriguez', 'Jimenez', 'Avenida de la Paz 4', 'ingeniero de vuelo', '1988-12-31', '147258369'),
+    (5, 'luis.garcia@mail.com', 'Luis', 'Garcia', 'Fernandez', 'Calle del Rio 5', 'azafato', '1995-08-18', '258369147'),
+    (6, 'ana.martin@mail.com', 'Ana', 'Martin', 'Lopez', 'Plaza de la Libertad 6', 'piloto', '1990-11-12', '963852741'),
+    (7, 'carlos.hernandez@mail.com', 'Carlos', 'Hernandez', 'Gomez', 'Calle del Mar 7', 'copiloto', '1986-03-07', '369147258'),
+    (8, 'carmen.sanchez@mail.com', 'Carmen', 'Sanchez', 'Perez', 'Avenida de la Constitucion 8', 'ingeniero de vuelo', '1991-05-22', '741852963'),
+    (9, 'antonio.fernandez@mail.com', 'Antonio', 'Fernandez', 'Santos', 'Calle del Pilar 9', 'azafato', '1994-09-28', '852963741'),
+    (10, 'laura.garcia@mail.com', 'Laura', 'Garcia', 'Gonzalez', 'Plaza de la Villa 10', 'piloto', '1989-12-24', '369258147'),
+    (11, 'daniel.perez@mail.com', 'Daniel', 'Perez', 'Hernandez', 'Calle del Carmen 11', 'copiloto', '1996-04-14', '258147369'),
+    (12, 'patricia.martinez@mail.com', 'Patricia', 'Martinez', 'Fernandez', 'Avenida de la Cruz 12', 'ingeniero de vuelo', '1987-07-11', '147369258'),
+    (13, 'alberto.sanchez@mail.com', 'Alberto', 'Sanchez', 'Rodriguez', 'Calle del Bosque 13', 'azafato', '1993-10-31', '963741852');
