d842eb4325e6c30fe888bad307092e2cd7dc4568
PedroRR-0 <117758296+PedroRR-0@users.noreply.github.com>
2023-05-08 10:38:15 +0200

    ini

--- src/vista/PestañaAsignacion.java
+++ src/vista/PestañaAsignacion.java
@@ -1,17 +1,40 @@
 package vista;
 
+import modelo.ConexionBD;
+
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PestañaAsignacion {
-    public void pestañaAsignacion(JPanel assignmentsPanel, JTabbedPane tabbedPane){
+    PestañaAsignacion(JTabbedPane tabbedPane) throws SQLException {
         // Pestaña de Asignación
-        assignmentsPanel = new JPanel ( );
+        JPanel assignmentsPanel = new JPanel();
         assignmentsPanel.setLayout ( new BorderLayout ( ) );
-        JTable      assignmentsTable      = new JTable ( );
-        JScrollPane assignmentsScrollPane = new JScrollPane ( assignmentsTable );
-        assignmentsPanel.add ( assignmentsScrollPane , BorderLayout.CENTER );
-        tabbedPane.addTab ( "Asignación" , assignmentsPanel );
+        JLabel tit = new JLabel("CONFIRMACIÓN DEL VUELO");
+        JPanel norte = new JPanel();
+        norte.add(tit);
+        assignmentsPanel.add(norte,BorderLayout.NORTH);
+        JPanel center = new JPanel();
+        JPanel centerLeft = new JPanel();
+        JPanel centerRight = new JPanel();
+        JLabel logistica = new JLabel("Logistica vuelo");
+        JLabel avion = new JLabel("AVIÓN: ");
+        ConexionBD con = new ConexionBD();
+        ResultSet result = con.ejecutarConsulta("SELECT matricula FROM aviones");
+        JComboBox<String> avionBox = new JComboBox<>();
+        while (result.next()){
+
+            avionBox.addItem(result.getString("matricula"));
+        }
+        centerLeft.add(logistica);
+        centerLeft.add(avion);
+        centerLeft.add(avionBox);
+        center.add(centerLeft);
+        center.add(centerRight);
+        assignmentsPanel.add(center,BorderLayout.CENTER);
+        tabbedPane.addTab ( "Asignación" , assignmentsPanel);
 
 
     }
