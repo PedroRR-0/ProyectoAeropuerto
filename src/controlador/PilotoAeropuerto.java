80288451a34b6a59264d65da2bf2f160eaa3aa9b
PedroRR-0 <117758296+PedroRR-0@users.noreply.github.com>
2023-05-09 14:24:10 +0200

    9 may fin

--- src/controlador/PilotoAeropuerto.java
+++ src/controlador/PilotoAeropuerto.java
@@ -4,10 +4,11 @@ import controlador.Login;
 import modelo.CargaDeDatos;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 
 public class PilotoAeropuerto {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Login metodosLogin = new Login ();
         CargaDeDatos Cargainicial           = new CargaDeDatos ();
         Connection   Conexioninicial          = metodosLogin.getConnection();
